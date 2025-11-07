d1c2f23b792265139445a6a0e143a2093712c952
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2025-11-07 10:02:34 -0300

    feat: mobile menu animation with frame motion

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,6 +2,8 @@ import { TextAlignJustifyIcon } from "lucide-react";
 import { sectionsId } from "../../shared/SectionsId";
 import type { MenuOption } from "../../@types/MenuOption";
 import { MenuOpt } from "./MenuOption";
+import { motion } from "motion/react";
+import { useState } from "react";
 
 
 const menuOptions: Array<MenuOption> = [
@@ -10,21 +12,61 @@ const menuOptions: Array<MenuOption> = [
   { label: 'Habilidades', refTo: sectionsId.skills },
   { label: 'Projetos', refTo: sectionsId.projects },
   { label: 'Serviços', refTo: sectionsId.services },
-  { label: 'Contato', refTo: sectionsId.services },
+  { label: 'Contato', refTo: sectionsId.contact },
 ];
 
 export function Menu() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
+  function openMenu() {
+    setIsMenuOpen(true);
+  }
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
+
   return (
     <header className="text-white relative">
-      <TextAlignJustifyIcon />
-      
-      <nav className="fixed h-screen w-[80%] max-w-[300px] top-0 py-10 bg-black/20">
+      <button onClick={openMenu}>
+        <TextAlignJustifyIcon />
+      </button>
+
+      {isMenuOpen && (
+        <div
+          className="block w-screen h-screen fixed top-0"
+          onClick={closeMenu}
+        ></div>
+      )}
+
+      <motion.nav
+        initial={{
+          x: '-100%',
+        }}
+        animate={{
+          x: isMenuOpen ? 0 : '-100%',
+        }}
+        transition={{ duration: 0.5 }}
+        className={`
+        fixed 
+        top-0  
+        z-50
+        
+        h-screen 
+        w-[80%] max-w-[300px] 
+        py-10 
+        
+        bg-black/50 
+        
+        `}>
         <ul>
           {menuOptions.map(opt => {
             return <MenuOpt option={opt} />
           })}
         </ul>
-      </nav>
-    </header>
+      </motion.nav>
+    </header >
   );
 }
