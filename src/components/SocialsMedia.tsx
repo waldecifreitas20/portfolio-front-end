176cc547b0725fa132b15552c1603ef477037e52
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2025-11-13 09:14:34 -0300

    refactor: split socials media section to resusable component

---
+++ src/components/SocialsMedia.tsx
@@ -0,0 +1,27 @@
+import gitHubIcon from "../assets/github-white-icon.svg";
+import linkedinIcon from "../assets/linkedin-white-icon.svg";
+
+const socials = [
+  { iconSrc: gitHubIcon, link: 'https://github.com/waldecifreitas20' },
+  { iconSrc: linkedinIcon, link: 'https://www.linkedin.com/in/waldeci-freitas21' },
+];
+
+export function SocialsMedia() {
+  return (
+    <article className="flex h-10 gap-2 w-fit mx-auto mb-4">
+      {socials.map(social => {
+        return (
+          <a
+            className="size-full opacity-70 hover:opacity-100"
+            href={social.iconSrc}
+            target="_blank"
+          >
+            <img className="size-full" src={social.iconSrc} alt="GitHub" />
+          </a>
+        );
+      })}
+
+    </article>
+
+  );
+}
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,72 +1,55 @@
-import { ContentArea } from "../components/ContentArea";
-import mobileBackground from "../assets/bg-hero-mobile.jpg";
-import { Menu } from "../components/Menu";
-import { useLanguage } from "../hooks/useLanguage";
-import gitHubIcon from "../assets/github-white-icon.svg";
-import linkedinIcon from "../assets/linkedin-white-icon.svg";
-import { Theme } from "../shared/Theme";
-import { Button } from "../components/Button";
-import { FlatButton } from "../components/FlatButton";
-import { DesktopBackground } from "../components/DesktopBackground";
+  import { ContentArea } from "../components/ContentArea";
+  import mobileBackground from "../assets/bg-hero-mobile.jpg";
+  import { Menu } from "../components/Menu";
+  import { useLanguage } from "../hooks/useLanguage";
+  import { Theme } from "../shared/Theme";
+  import { Button } from "../components/Button";
+  import { FlatButton } from "../components/FlatButton";
+  import { DesktopBackground } from "../components/DesktopBackground";
+import { SocialsMedia } from "../components/SocialsMedia";
 
-export function HomeSection() {
-  const { content } = useLanguage();
-  const { hero } = content;
+  export function HomeSection() {
+    const { content } = useLanguage();
+    const { hero } = content;
 
-  return (
-    <section
-      className="h-screen w-full overflow-hidden relative"
-      style={{
-        background: `url(${mobileBackground}) no-repeat top center`,
-        backgroundSize: 'cover',
-      }}>
-      <DesktopBackground />
-      <ContentArea tailwindCss="py-2 relative z-10">
-        <Menu />
-        <section className="text-white font-bold text-center mt-20">
+    return (
+      <section
+        className="h-screen w-full overflow-hidden relative"
+        style={{
+          background: `url(${mobileBackground}) no-repeat top center`,
+          backgroundSize: 'cover',
+        }}>
+        <DesktopBackground />
+        <ContentArea tailwindCss="py-2 relative z-10">
+          <Menu />
+          
+          <section className="text-white font-bold text-center mt-20">
+            <SocialsMedia />
+            <article>
+              <p className="text-2xl" style={{
+                color: Theme.accent
+              }}>{hero.title}</p>
+              <h1 className="text-5xl mt-2 mb-4">Waldeci Freitas</h1>
+              <p className="font-normal text-white/70">{hero.intro}</p>
+            </article>
 
-          <article className="flex h-10 gap-2 w-fit mx-auto mb-4">
-            <a
-              className="size-full opacity-70 hover:opacity-100"
-              href="https://github.com/waldecifreitas20"
-              target="_blank"
-            >
-              <img className="size-full" src={gitHubIcon} alt="GitHub" />
-            </a>
-            <a
-              className="size-full opacity-70 hover:opacity-100"
-              href="https://www.linkedin.com/in/waldeci-freitas21"
-              target="_blank"
-            >
-              <img className="size-full" src={linkedinIcon} alt="Linkedin" />
-            </a>
-          </article>
+            <article
+              className="
+              flex flex-col 
+              gap-4 my-8 
+              
+              md:flex-row 
+              md:max-w-[450px] 
+              md:mx-auto
+              
+              ">
+              <Button>{hero.buttons.projects}</Button>
+              <FlatButton>{hero.buttons.contact}</FlatButton>
+            </article>
 
-          <article>
-            <p className="text-2xl" style={{
-              color: Theme.accent
-            }}>{hero.title}</p>
-            <h1 className="text-5xl mt-2 mb-4">Waldeci Freitas</h1>
-            <p className="font-normal text-white/70">{hero.intro}</p>
-          </article>
+          </section>
 
-          <article
-            className="
-            flex flex-col 
-            gap-4 my-8 
-            
-            md:flex-row 
-            md:max-w-[450px] 
-            md:mx-auto
-            
-            ">
-            <Button>{hero.buttons.projects}</Button>
-            <FlatButton>{hero.buttons.contact}</FlatButton>
-          </article>
-
-        </section>
-
-      </ContentArea>
-    </section>
-  );
-}
+        </ContentArea>
+      </section>
+    );
+  }
